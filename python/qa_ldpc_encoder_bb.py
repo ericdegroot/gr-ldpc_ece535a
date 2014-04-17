4386ba7ad3b1c037770d92689f8e185a4070cc0b
Eric de Groot <edegroot@email.arizona.edu>
2014-04-17 15:59:21 -0700

    Removed misplaced text from file

--- python/qa_ldpc_encoder_bb.py
+++ python/qa_ldpc_encoder_bb.py
@@ -20,8 +20,6 @@ class qa_ldpc_encoder_bb (gr_unittest.TestCase):
     def test_001_t (self):
         # data and check bits generated in MATLAB
         data  = ( 0b11101101, 0b00000010, 0b00110100, 0b10000000, 0b01010011, 0b00000110, 0b00110001, 0b11101000 )
-0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0,
-
         check = ( 0b10110100, 0b10101001, 0b11010101, 0b10001000, 0b01010000, 0b01011001, 0b10010000, 0b11110001 )
 
         # expected result is alternating check,data,check,data,... bytes
